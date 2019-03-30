dd1374a2c196fa68b4331f5715fc6a3ca1aed22f
yuki <marukesu11@icloud.com>
2019-03-31 06:06:53 +0900

    Initial Commit

---
+++ segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by 川田有紀 on 2019/03/31.
+//  Copyright © 2019 jp.techacademy.yuki.kawata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
